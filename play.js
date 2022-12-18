a2ac72b751b2da44a605123d26f6f4caa378849f
Nour Mohamoud <nour.mohamoud9@gmail.com>
2022-12-18 01:06:36 +0000

    add: play.js

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "10.0.2.15", // IP address here,
+    port: "50541", // PORT number here,
+  });
+
+  conn.on("connect", () => {
+    console.log("I'm connected!"); // code that does something when the connection is first established
+  });
+  conn.on("data", (data) => {
+    console.log(data); // code that does something when the connection is first established
+  });
+  // interpret incoming data a  s text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
