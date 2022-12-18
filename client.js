29bf7e17e4341bf97a0b5b21a44c437f1d1cf9fb
Nour Mohamoud <nour.mohamoud9@gmail.com>
2022-12-18 01:48:58 +0000

    update: client.js

--- client.js
+++ client.js
@@ -8,11 +8,13 @@ const connect = function () {
   });
 
   conn.on("connect", () => {
-    console.log("I'm connected!"); // code that does something when the connection is first established
+    console.log("Successfully I'm connected!"); // code that does something when the connection is first established
   });
+  conn.write("Name: NYM");
   conn.on("data", (data) => {
     console.log(data); // code that does something when the connection is first established
   });
+ 
   // interpret incoming data a  s text
   conn.setEncoding("utf8");
 
